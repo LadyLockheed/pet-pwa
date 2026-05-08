fbc653d0283c4877b9ecd0eff520bc09bf1c57ea
Karin Karlsson <karin.drake@gmail.com>
2026-05-08 19:34:28 +0200

    Implement PetProfile in App with mockeddata

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import Layout from './components/Layout';
+import PetProfile from './components/PetProfile';
+import { petType, type Pet } from './db';
+
+const pet: Pet = {
+	id: '1',
+	name: 'Nora',
+	species: petType.dog,
+	breed: 'Bichon Havanais',
+	age: 4,
+	createdAt: new Date().toISOString(),
+	updatedAt: new Date().toISOString(),
+};
+
 function App() {
-  return <div>pet pwa</div>
+	return (
+		<Layout>
+			<PetProfile pet={pet} />
+		</Layout>
+	);
 }
 
-export default App
+export default App;
