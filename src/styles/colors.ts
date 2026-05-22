6de24e88e9c215d6fcf4b8ba5f8162579685251a
Karin Karlsson <karin.drake@gmail.com>
2026-05-22 11:31:08 +0200

    Set up styling files

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+	background: '#4e2e1b',
+	warmWhite: '#fcfaf7',
+	blackBrown: '#2f190f',
+	warmBrown: '#5a3118',
+	coldBrown: '#64554b',
+	orange: '#d76900',
+	lightBeige: '#f2eae1',
+	darkBeige: '#ded6cd',
+	white: '#FFFFF',
+};
---
+++ src/styles/spacings.ts
@@ -0,0 +1,8 @@
+export const spacings = {
+	x05: '2px',
+	x1: '4px',
+	x2: '8px',
+	x3: '12px',
+	x4: '16px',
+	x5: '20px',
+};
