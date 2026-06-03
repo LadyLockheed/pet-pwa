56e7a8d9f68a9d2566782561e915606678ef0166
Karin Karlsson <karin.drake@gmail.com>
2026-06-03 23:08:00 +0200

    Add emptystate view in petsoverview

--- src/components/PetsOverview.tsx
+++ src/components/PetsOverview.tsx
@@ -1,28 +1,67 @@
 import styled from 'styled-components';
 import PetCard from './PetCard';
 import type { Pet } from '../types/pet';
+import { spacings } from '../styles/spacings';
+import { colors } from '../styles/colors';
+import { Link } from 'react-router-dom';
+import { CirclePlus } from 'lucide-react';
 
 interface PetsOverviewProps {
 	pets: Pet[];
 }
 
 export default function PetsOverview({ pets }: PetsOverviewProps) {
-	if (pets.length === 0) {
-		return <div>Du har inte lagt in några djur än</div>;
-	}
 	return (
 		<Overview>
-			{pets.map((pet) => (
-				<PetCard key={pet.id} pet={pet} />
-			))}
+			<Headline>
+				Tassar<span style={{ color: colors.orange }}>&</span>Nosar
+			</Headline>
+
+			{pets.length === 0 ? (
+				<EmptyStateContent>
+					<EmptyStateText>
+						Här var det tomt. Klicka på + för att lägga till en pälskling
+					</EmptyStateText>
+					<AddPetLink to="/add-pet">
+						<CirclePlus size={32} />
+					</AddPetLink>
+				</EmptyStateContent>
+			) : (
+				pets.map((pet) => <PetCard key={pet.id} pet={pet} />)
+			)}
 		</Overview>
 	);
 }
 
 const Overview = styled.section({
 	display: 'grid',
-	gap: '16px',
+	gap: spacings.x4,
 	width: '100%',
-	maxWidth: '720px',
 	boxSizing: 'border-box',
+	padding: spacings.x3,
+	minHeight: 'calc(100vh - 120px)',
+});
+
+const EmptyStateContent = styled.div({
+	display: 'grid',
+	justifyItems: 'center',
+	alignContent: 'center',
+	gap: spacings.x4,
+	textAlign: 'center',
+});
+
+const EmptyStateText = styled.div({
+	color: colors.warmWhite,
+});
+
+const AddPetLink = styled(Link)({
+	color: colors.orange,
+});
+
+const Headline = styled.h1({
+	fontFamily: "'EB Garamond', Georgia, serif",
+	fontWeight: 700,
+	color: colors.background,
+	margin: 'auto',
+	marginBottom: spacings.x6,
 });
